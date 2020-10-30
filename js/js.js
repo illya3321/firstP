3f2d9b6e5a754730bd37dd3031a2d112fdda1d0b
illya3321 <illya33231@gmail.com>
2020-10-30 15:01:49 +0200

    js

---
+++ js/js.js
@@ -0,0 +1,217 @@
+//---1
+//let greating =`hello`;
+//let site = `owu`;
+//let domain = `com`;
+//let bigcountry = `ua`;
+//let firstdaymonth = 1;
+//let windows = 10;
+//let symbols = -999;
+//let bags = 123;
+//let term = 3.14;
+//let bar = 2.7;
+//let age = 16; 
+//let iamillya = true;
+//let isgirl = false
+
+
+//alert ( greating );
+//document.write( greating );
+//console.log( greating );
+
+//alert( site );
+//document.write( site );
+//console.log( site );
+
+//alert( domain );
+//document.write( domain );
+//console.log( domain );
+
+//alert( bigcountry );
+//document.write( bigcountry );
+//console.log( bigcountry );
+
+//alert( firstdaymonth );
+//document.write( firstdaymonth );
+//console.log( firstdaymonth );
+
+//alert( windows );
+//document.write( windows );
+//console.log( windows )
+
+//alert( symbols );
+//document.write( symbols );
+//console.log( symbols );
+
+//alert( bags );
+//document.write( bags );
+//console.log( bags );
+
+//alert( term );
+//document.write( term );
+//console.log( term );
+
+//alert( bar );
+//document.write( bar );
+//console.log( bar );
+
+//alert( age );
+//document.write( age );
+//console.log( age );
+
+//alert( iamillya );
+//document.write( iamillya );
+//console.log( iamillya );
+
+//alert( isgirl );
+//document.write( isgirl );
+//console.log( isgirl );
+//-----------------------------------------------------
+
+//------2
+
+//hello = `hi`;
+//owu = `shool`;
+//com = `zzz`;
+//ua = `ukraine`;
+//firstdaymonth = `first`;
+//windows = `list`;
+//symbols = `negative`;
+//bags = `123123`;
+//term = `pi`;
+//bar = `some`;
+//age = `years`;
+//iamillya = `false`;
+//isgirl = `true`;
+
+//console.log( hello, owu, com, ua, firstdaymonth, windows, symbols, bags, term, bar, age, iamillya, isgirl );
+//document.write( hello + " "+ owu+" "+ com+" "+ ua+" "+firstdaymonth+" "+ windows+" "+ symbols+" "+ bags+" "+ term+" "+bar+" "+age+" "+iamillya+" "+isgirl); 
+//alert(hello);
+//alert(owu);
+//alert(com);
+//alert(ua);
+//alert(firstdaymonth);
+//alert(windows);
+//alert(symbols);
+//alert(bags);
+//alert(term);
+//alert(bar);
+//alert(age);
+//alert(iamillya);
+//alert(isgirl);
+//------------------------------------------
+
+
+//----3
+
+//const one = 1;
+//const two = 2;
+//const three = 3;
+//const firstword = `qwerty`; 
+//const secondword = `qqqq`;
+//const thirdword = `xxxxx`;
+
+  //alert( one );
+  //document.write( one );
+  //console.log( one );
+
+  //alert( two );
+  //document.write( two );
+  //console.log( two );
+
+  //alert( three );
+  //document.write( three );
+  //console.log( three );
+
+    //alert( firstword );
+    //document.write( firstword );
+    //console.log( firstword );
+
+    //alert( secondword );
+    //document.write( secondword );
+    //console.log( secondword );
+
+    //alert( thirdword );
+    //document.write( thirdword );
+    //console.log( thirdword );
+
+//----------------------------------------------------
+
+//---4-------
+
+//let name = prompt(`my name`);
+//let surname = prompt (`my surname`);
+//let fathersname = prompt(`name of father`);
+
+   //alert( name );
+   //document.write(name);
+   //console.log(name);
+
+   //alert( surname );
+   //document.write(surname);
+   //console.log(surname);
+
+   //alert( fathersname );
+   //document.write(fathersname);
+   //console.log(fathersname);
+
+
+
+     //let person = ` name surname fathersname `;
+
+     //-----------------------------------------------------------------------
+
+//6-------------------------------------------------------
+
+    //let name = prompt(` Enter Fathers name`);
+    //let surname = prompt(`Enter Fathers surname`);
+    //let fathersname = prompt(` Enter Fathers father name`);
+    //let Father = ` ${name} ${surname} ${fathersname}`;
+
+
+       //console.log( 'Father:' )
+       //console.log(name);
+       //console.log(surname);
+       //console.log(fathersname);
+
+    //let nameof = prompt(` Enter Mothers name`);
+    //let surnameof = prompt(`Enter Mothers surname`);
+    //let fathersnameof = prompt(` Enter Fathers mother name`);
+    //let Mother = ` ${nameof} ${surnameof} ${fathersnameof}`;
+
+      //console.log( 'Mother:' )
+      //console.log(nameof);
+      //console.log(surnameof);
+      //console.log(fathersnameof);
+    
+    //let nameby = prompt(` Enter Grandmothers name`);
+    //let surnameby = prompt(`Enter Grandmothers surname`);
+    //let fathersnameby = prompt(` Enter Fathers grandmother name`);
+    //let Grandmother = ` ${nameby} ${surnameby} ${fathersnameby}`;
+  
+      //console.log( 'Grandmother:' )
+      //console.log(nameby);
+      //console.log(surnameby);
+      //console.log(fathersnameby);  
+
+
+//-----------------------------------------------------------------------------------
+//7---------------------------------------
+
+      //let numberone = prompt(`Enter first number`);
+      //let numbertwo = prompt(`Enter second number`);
+      //let numberthree = prompt(`Enter third number`);
+        //console.log( +numberone, + numbertwo, + numberthree );
+//---------------------------------------------
+
+
+
+    
+   
+
+
+
+
+
+
+
+
