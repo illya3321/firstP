51fd1d1adf258d052aa82a2563929f50fd89ccb7
illya3321 <illya33231@gmail.com>
2020-11-03 11:39:33 +0200

    js2

---
+++ homework/js2/js2.js
@@ -0,0 +1,123 @@
+//створити масив та вивести його в консоль:
+//з 5 числових значень
+//з 5 стічкових значень
+//5 значень стрічкового, числового та булевого типу
+//Створити пустий масив. Наповнити його будь-якими значеннями звертаючись до конкретного індексу. Вивести в консоль
+
+
+
+
+
+//1.2.3.4.5---------------------------------
+//let number = [1, 2, 3, 4, 5] ;
+//console.log(number);
+
+//let string = [ "illya", "dima", "pavlo", "vasyl", "nazar" ] ;
+//console.log(string);
+
+//let different =  [ "andriy", 45, false, "katya", 333 ];
+//console.log(different);
+
+//let zero = [];
+ //zero[1] = true;
+ //zero[2] = 123; 
+ //zero[3] = { name:"illya", age:"20", car:false };
+ //zero[4] = "somesing interesting";
+ //zero[5] = "nothing";
+ //zero[6] = 
+
+ //console.log(zero);
+//------------------------------------------------------
+
+
+//7.8------------------------------------------------------
+//for( let i=0; i<10; i++) {
+//document.write("<div>test text</div>"); } 
+
+//for( let i=0; i<10; i++) {
+//document.write(`<div>test text : ${i} </div>`); }
+ 
+//------------------------------
+
+
+//9.10--------------------------
+
+ 
+//let i = "0" 
+//while (i < 20) {
+//document.write("<h1> i love meat </h1>");
+//i++;}
+
+//let a = "1"
+//while (a < 21){
+//document.write(`<h1> i love meat : ${a} </h1>`);
+//a++;} 
+//-------------------------
+
+
+//11------------------
+
+//let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+//for (let number of [1,2,3,4,5,6, 7, 8, 9, 10]) {
+    //console.log(number);
+//}
+//-------------
+
+
+//12-----------------
+ //const string = ( "illya", "someone", "who", "are", "you", "and", "where", "you", "from", "lviv")
+ //for (let j =0; j < string.length; j++ ) {
+    //console.log(string[j] ) }
+
+//-------------------------------------
+
+//---14.15.16-----------------------------
+
+
+ //if (typeof 87 );
+ //if (typeof 34 );
+ //if (typeof 42 );
+ //if (typeof 99 );
+ //if (typeof false );
+ //if (typeof true );
+ //if (typeof false );
+ //if (typeof "illya" );
+ //if (typeof "nazar" );
+ //if (typeof "dima" );
+
+  
+
+
+  //console.log(typeof  false);
+  //console.log(typeof true);
+  //console.log(typeof false);
+
+   //console.log(typeof 87);
+   //console.log(typeof 34);
+   //console.log(typeof 42);
+   //console.log(typeof 99);
+     
+     //console.log(typeof 'illya');
+     //console.log(typeof 'nazar');
+     //console.log(typeof 'dima');
+
+//---------------------------------------
+
+
+//-18.19------------------------
+
+//for( let i=0; i<10; i++) {
+//document.write( ` step : ` , i  ); 
+//console.log(`step  ${i}` ); }
+
+//for( let i=0; i<100; i++)  {
+    //document.write( ` step :  `, i);
+    //console.log(`step ${i}`); }
+
+
+
+//-----------------------
+
+
+
+
