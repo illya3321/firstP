93cacafe2c81e92571f80944cc2235816aee30b4
illya3321 <illya33231@gmail.com>
2020-11-03 11:43:20 +0200

    js3

---
+++ homework/js3/js3.js
@@ -0,0 +1,136 @@
+//створити об'єкт (не меньше 5ти властивостей) який описує
+//собаку
+//людину
+//автомобіль
+//квартиру
+//книгу
+
+//1------------
+//let dog = {
+    //name : "tina",
+    //sex : "female",
+    //breed : "German Shepherd",
+    //achievement : "First Places",
+    //skills : "stage 3",
+    //}  
+    //console.log( dog );
+    
+    
+    
+    //let person = {
+    //name : "illya",
+    //age : 20,
+    //skills : "HTML, CSS",
+    //hasGirl : true,
+    //study : "OK10",
+    //}
+    //console.log(person);
+    
+    
+    
+    //let car = {
+    //brand : "BMW",
+    //type : "coupe",
+    //fuel : "diezel",
+    //security : "keyless access",
+    //roadAccident : false ,
+    //}
+    //console.log(car);
+    
+    
+    
+    //let flat ={
+    //cost : 10000 ,
+    //area : "cuxiv",
+    //type : "new house",
+    //floor : 7 ,
+    //demencions : " three bedroom",
+    //}
+    //console.log(flat);
+    
+    
+    
+    //let book ={
+    //type : "new",
+    //cost : 50 , 
+    //genre : "fantastic",
+    //pages : 800 , 
+    //author : "unknown",
+    //}
+    //console.log(book);
+//----------------------   
+
+
+//2---------------------------
+//let dogs = [ "dog1", "dog2", "dog3", "dog4", "dog5" ];
+//console.log(dogs);
+
+
+//let people = [ "person1", "person2", "person3", "person4", "person5" ];
+//console.log(people);
+
+
+//let cars = [ "BMW", "MERCEDES-BENZ", "AUDI", "KIA", "HONDA" ];
+//console.log(cars);
+//---------------------------------
+
+
+//3-------------------------
+
+
+//let house = {
+//square : "30",
+//location : "outside the city",
+//floors : "2",
+//teritory : "pavement",
+//garage : true,
+//}
+//console.log(house)
+
+
+
+//let driver =[
+//"expiriense-20 years", "work-taxi", "workingDay-10hours", "endedUniversity-NLTU", "hasChildre"
+//]
+//console.log(driver)
+
+//------------------------------
+
+
+
+
+
+//4-----------------------------------------------------------------------------------------
+//let users = [
+    //{name: 'vasya', age: 31, status: false},
+    //{name: 'petya', age: 30, status: true},
+    //{name: 'kolya', age: 29, status: true},
+    //{name: 'olya', age: 28, status: false},
+    //{name: 'max', age: 30, status: true},
+    //{name: 'anya', age: 31, status: false},
+    //{name: 'oleg', age: 28, status: false},
+    //{name: 'andrey', age: 29, status: true},
+    //{name: 'masha', age: 30, status: true},
+    //{name: 'olya', age: 31, status: false},
+    //{name: 'max', age: 31, status: true}
+//];
+
+//console.log( users[7] .status  );
+//console.log( users[4] .status );
+//console.log( users[9] .name );
+//console.log( users[3] .name );
+//console.log( users[6] .age );
+//console.log( users[3] .age );
+//console.log( users[5] .age , name  );
+//console.log( users[5] .status );
+//----------------------------------
+
+
+
+
+
+
+
+
+
+
