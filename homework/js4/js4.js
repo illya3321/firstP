199007013bfe183904842980cebca58d1b64c0e5
illya3321 <illya33231@gmail.com>
2020-11-12 15:03:39 +0200

    js4

---
+++ homework/js4/js4.js
@@ -0,0 +1,398 @@
+//1.3.4---------------------------------------
+//let person = {
+//name : 'illya' ,
+//age : 20 ,
+//education : 'NLTU' , 
+//};
+//console.log(person);
+//for(key in person) {
+    //console.log (key);
+//}
+//console.log(Object.keys(person));
+
+//let q = JSON.stringify(person);
+//console.log(q);
+//let w =JSON.parse(q) ;
+//console.log(w);
+
+
+
+
+
+//let friend = {
+//work : 'programer' ,
+//skills : 'java' , 
+//liveIn : 'lviv' ,
+//}
+//console.log(friend);
+//for(key in friend) {
+    //console.log(key);
+//}
+//console.log(Object.keys(friend));
+
+//let z = JSON.stringify(friend) ;
+//console.log(z);
+//let v = JSON.parse(z) ;
+//console.log(v) ;
+
+
+
+
+//let car = {
+//model : 'tesla' , 
+//type : 'sedan' ,
+//fuel : 'electricity',
+//}
+//console.log(car);
+//for(key in car){
+    //console.log(key);
+//}
+//console.log(Object.keys(car));
+
+//let zminna = JSON.stringify(car) ;
+//console.log(zminna) ;
+//let something = JSON.parse(zminna) ;
+//console.log(something) ;
+
+
+
+
+
+//let phone ={
+//camera : 12 ,
+//batery : 2500 ,
+//conector : 'type-c' ,
+//}
+//console.log(phone);
+//for(key in phone) {
+    //console.log(key);
+//}
+//console.log(Object.keys(phone));
+//let top = JSON.stringify(phone) ;
+//console.log(top) ;
+//let back = JSON.parse(top) ;
+//console.log(back) ;
+
+
+
+
+
+//let laptop = {
+//corpus : 'metal' ,
+//ethernet : true ,
+//videoCard : 'nvidia' ,
+//}
+//console.log(laptop);
+//for(key in laptop) {
+    //console.log(key);
+//}
+//console.log(Object.keys(laptop));
+//let test = JSON.stringify(laptop) ;
+//console.log(test) ;
+//let drop = JSON.parse(test) ;
+//console.log(drop) ;
+
+//--------------------------------------------------
+
+
+//2.3.4--------------------------------------------
+
+
+//let son = {
+//name : 'maks' ,
+//age : 25 ,
+//hasGirlfriend : { 
+    //name : 'ira' , 
+    //age : 20 } ,
+//skills : ['javaskript' , 'java' ] 
+//}
+//console.log(son);
+//for(key in son) {
+    //console.log (key);
+//}
+//console.log(Object.keys(son));
+
+
+
+
+//let house = {
+//area : 'cuxiv' ,
+//floors : ['first' , 'second'] ,
+//conviniances : { 
+    //ekectricity : true ,
+    //gas : false ,
+    //water : false   }
+//}
+//console.log(house);
+//for(key in house) {
+    //console.log(key);
+//}
+//console.log(Object.keys(house));
+
+
+
+//let car = {
+    //model : 'nissan' , 
+    //type : 'sedan' ,
+    //fuel : [ 'patrol' , 'gas'] ,
+    //seasonsonroad : {
+        //winter : true ,
+        //summer : false ,  }
+    //}
+    //console.log(car);
+    //for(key in car) {
+        //console.log(key);
+    //}
+//console.log(Object.keys(car));
+
+
+
+//let laptop = {
+    //corpus : ['metal' , 'plastic'] ,
+        //ethernet : true ,
+        //videoCard : { 
+            //intel : true , 
+            //amd : true,  }
+        //}
+//console.log(laptop);
+//for(key in laptop) {
+    //console.log(key);
+//}
+//console.log(Object.keys(laptop));
+
+
+
+
+//let me = {
+//name : 'illya' ,
+//age : 20 ,
+//hobby : [ 'cars' , 'bikes', 'hunting' ] ,
+//owns : {
+    //car : false ,
+    //bike : false ,   }
+//}
+//console.log(me);
+//for(key in me) {
+    //console.log(key);
+//}
+//console.log( Object.keys(me) );
+
+//----------------------------------------------
+
+
+
+
+//5----------------------------------------------
+//let massiveCars = [ 
+    //{car : "tesla" , model : "X" ,type : "crossover" ,graduationYear : 2020 ,  } ,
+
+    //{car : "kia" , model : "ceed" ,type : "universal" ,graduationYear : 2012 , } ,
+
+    //{car : "jeep" , model : "cheeroke" , type : "off-road" , graduationYea : 2005 , } ,
+
+    //{car : "opel" ,model : "asrta" ,type : "hatchback" ,graduationYear : 2010 , } ,
+
+    //{car : "volkswagen" , model : "golf" , type : "cope" , graduationYear : 2000 , } ,
+
+    //{car : "lamborgini" , model : "huracan" , type : "sportcar" , graduationYear : 2020 ,  } ,
+
+    //{car : "nissan" , model : "gtr" , type : "cope" , graduationYear : 2018 , } ,
+
+    //{car : "porshe" , model : "cayene" , type : "off-road" , graduationYear : 2005 , } ,
+
+    //{car : "dacia" , model : "logan" , type : "hatchback" , graduationYear : 2007 , } ,
+
+    //{car : "toyota" , model : "camry" , type : "sedan" , graduationYear : 2018 , } ,
+
+    //]
+//console.log(massiveCars);
+
+//let car = 0;
+//while(car < massiveCars.lenght) {
+//car++}
+
+//for(let a =0; a < massiveCars.length; a++ ){
+//console.log(massiveCars[a]);  }
+
+
+
+
+
+
+
+//let massiveCities = [
+    //{city : "lviv" , population : 1500000, traffic : 7/10 , turists : true , } ,
+
+    //{city : "ternopil" , population : 500000, traffic : 3/10 , turists : false , } ,
+
+    //{city : "kyiv" , population : 3000000, traffic : 10/10 , turists : true , } ,
+
+    //{city : "lutsk" , population : 150000, traffic : 5/10 , turists : false , } ,
+
+    //{city : "odessa" , population : 250000, traffic : 9/10 , turists : true , } ,
+
+    //]
+//console.log(massiveCities);
+
+//let cities = 0;
+//while( cities< massiveCities.length){
+//cities++;}
+
+//for(let cities =0; cities< massiveCities.length; cities++ ){
+//console.log(massiveCities[cities]);  }
+
+
+
+
+
+
+//let massiveDrivers = [
+
+    //{car : "tesla" , model : "X" ,type : "crossover" ,graduationYear : 2020 , diver : {
+      //   name : "illya" , age : 20 , sex : "male" , skills : 3 , }  } ,
+
+
+    //{car : "kia" , model : "ceed" ,type : "universal" ,graduationYear : 2012 , driver :{
+      //  name : "andriy" , age : 30 , sex : "male" , skills : 10 ,} } ,
+
+
+    //{car : "jeep" , model : "cheeroke" , type : "off-road" , graduationYea : 2005 , driver : {
+      //  name : "dima" , age : 50 , sex : "male" , skills : 25 ,} } ,
+
+
+    //{car : "opel" ,model : "asrta" ,type : "hatchback" ,graduationYear : 2010 , driver : {
+      //  name : "nazar" , age : 45 , sex : "male" , skills : 10 ,} } ,
+
+
+    //{car : "volkswagen" , model : "golf" , type : "cope" , graduationYear : 2000 , driver : {
+      //  name : "maks" , age : 35 , sex : "male" , skills : 5 ,} } ,
+
+    //]
+
+//console.log(massiveDrivers);
+//let i = 0;
+//while( i<massiveDrivers.length ){i++;}
+
+//for(let z=0; z < massiveDrivers.length; z++ ) {
+//console.log(massiveCars[z]) }
+
+
+//let newtest = [] ;
+//for (let i = 0; i < massiveDrivers.length; i++) {
+    //const driver = massiveDrivers[i];
+    //newtest [i] = JSON.parse(JSON.stringify(driver)) ;
+//}
+//console.log(newtest);
+
+//16--------------------------------------------------------
+    //let users = [
+    //user = { job : 'developer' , skils : ['js' ,'css' ] } ,
+    //user = { job : 'front-end' , skils : ['html', 'css' ] } ,
+    //user = { job : 'tester' , skils : ['sql' ,  'css' ] } , ]
+
+//for ( user of users ) {
+//console.log(users) ;
+//for ( users of user. skils ); {
+    //console.log(user.skils);
+//}
+//}
+
+//20-----------------------------------------
+
+//let users = [
+  //  {name: 'vasya', age: 31, status: false, skills: ['java', 'js']},  
+
+  //  {name: 'petya', age: 30, status: true, skills: ['java', 'js', 'html']}, 
+
+  //  {name: 'kolya', age: 29, status: true, skills: ['mysql', ',mongo']}, 
+
+  //  {name: 'olya', age: 28, status: false, skills: ['java', 'js']}, 
+
+  //  {name: 'max', age: 30, status: true, skills: ['mysql', ',mongo']}      ];
+
+  // for (skills of users) {
+    //   console.log(users) ;
+   // for ( users of skills );{
+     //   console.log(skills)
+    //}
+   //}
+
+//-------------------------------------------------
+
+
+
+//24-------------------------------------
+
+
+//let users = [{
+  //  name: 'vasya',
+  //  age: 31,
+  //  status: false,
+  //  address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+//}, {
+  //  name: 'petya',
+  //  age: 30,
+  //  status: true,
+  //  address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+//}, {
+  //  name: 'kolya',
+  //  age: 29,
+  //  status: true,
+  //  address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+//}, {
+  //  name: 'olya',
+    //age: 28,
+    //status: false,
+    //address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+//}, {
+  //  name: 'max',
+  //  age: 30,
+  //  status: true,
+  //  address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+//}, {
+  //  name: 'anya',
+  //  age: 31,
+  //  status: false,
+  //  address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+//}, {
+  //  name: 'oleg',
+  //  age: 28,
+  //  status: false,
+  //  address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+//}, {
+  //  name: 'andrey',
+  //  age: 29,
+  //  status: true,
+  //  address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+//}, {
+  //  name: 'masha',
+  //  age: 30,
+  //  status: true,
+  //  address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+//}, {
+  //  name: 'olya',
+  //  age: 31,
+  //  status: false,
+  //  address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+//}, {
+  //  name: 'max',
+  //  age: 31,
+  //  status: true,
+  //  address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+//}];
+
+//let adres = [];
+//for ( user of users ) {
+   //  adres.push(user.address)  }
+//console.log(adres);
+
+
+
+
+
+
+
+
+
+
+
